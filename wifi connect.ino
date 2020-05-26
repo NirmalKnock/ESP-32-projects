d91284b41dc6671e3613aca8aa07b34569d1989d
Nirmal Kumar <54719308+NirmalKnock@users.noreply.github.com>
2020-05-26 18:31:06 +0530

    Create wifi connect.ino

---
+++ wifi connect.ino
@@ -0,0 +1,26 @@
+#include<WiFi.h>>
+const char ssid[]="nirmal kumar";
+const char password[]="nirmal@1234";
+void setup() {
+  // Initialize serial
+Serial.begin(9600);
+delay(100);
+// connect wifi
+Serial.print("connecting to wifi");
+Serial.print("ssid");
+WiFi.begin(ssid,password);
+while(WiFi.status()!=WL_CONNECT){
+  delay(500);
+  Serial.print(".");
+  
+}
+Serial.println():
+//Show connected
+Serial.println("connected");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
